d546bfe0f8c9f0ff337076513b8a975168891873
Asta1984 <salilmandal908@gmail.com>
2025-03-01 22:18:17 +0530

    deprived of errors

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import "@/styles/animations.css"
-
+import {Buffer} from 'buffer';
+window.Buffer = Buffer;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
